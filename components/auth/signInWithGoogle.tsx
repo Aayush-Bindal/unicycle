4f02a3b7a974ea0e94fbf513248d4de52f918e05
Aayush Bindal <176171584+Aayush-Bindal@users.noreply.github.com>
2025-11-13 13:02:22 +0530

    feat(auth): add functionality to button

--- components/auth/signInWithGoogle.tsx
+++ components/auth/signInWithGoogle.tsx
@@ -8,7 +8,7 @@ export default function SignInWithGoogle() {
 
   return (
     <button
-      onClick={() => { handleSignIn }}
+      onClick={() => { handleSignIn() }}
       className="relative flex items-center justify-center gap-3 w-full max-w-xs py-3 rounded-xl 
                  backdrop-blur-md bg-white/5 hover:bg-white/10 border border-white/20 
                  text-white font-medium transition-all duration-300 shadow-lg
