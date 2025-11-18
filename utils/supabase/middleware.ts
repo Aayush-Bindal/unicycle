253d8923047bac1d0f07ec4c2992c8fe60a7a55f
Aayush Bindal <176171584+Aayush-Bindal@users.noreply.github.com>
2025-11-19 02:08:16 +0530

    style: format middleware code

--- utils/supabase/middleware.ts
+++ utils/supabase/middleware.ts
@@ -6,8 +6,6 @@ export async function updateSession(request: NextRequest) {
     request,
   })
 
-  console.log("update")
-
   const supabase = createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!,
@@ -41,7 +39,6 @@ export async function updateSession(request: NextRequest) {
 
   if (
     !user &&
-    !request.nextUrl.pathname.startsWith('/login') &&
     !request.nextUrl.pathname.startsWith('/auth') &&
     !request.nextUrl.pathname.startsWith('/error')
   ) {
